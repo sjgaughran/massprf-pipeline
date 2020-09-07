601b4a1c91e9bc09367ed3fd90759c4a362c2ca4
Ubuntu <ubuntu@ip-172-31-20-67.us-east-2.compute.internal>
2020-09-07 01:28:47 +0000

    Add python version requirement

---
+++ README.md
@@ -0,0 +1,2 @@
+# massprf-pipeline
+
--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup, find_packages
+import sys
+
 setup(
     name='PMRF',
     version='0.1',
+    python_requires=">=3.3",
     packages=['PreMassPrf'],
     include_package_data=True,
     install_requires=[
